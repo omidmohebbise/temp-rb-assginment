82c97731ff2060c2812a83ab450dbb2c841e25e3
omid mohebbi <omidmohebbise@gmail.com>
2024-02-09 11:13:46 +0100

    Initial authentication spring app.

---
+++ assignment-authentication-api/src/main/java/com/rbank/authenticatoin/AssignmentAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.rbank.authenticatoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentAuthenticationApiApplication.class, args);
+    }
+
+}
---
+++ assignment-authentication-api/src/test/java/com/rbank/authenticatoin/AssignmentAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbank.authenticatoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentAuthenticationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
